d7898cd1eefba8fe418184c179a3ee2ae8d37f3c
amarelowo <willianaqw123@gmail.com>
2023-03-28 21:01:34 -0300

    same changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,52 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-
+// Pin
 #define PINLED 2
+
+// Pino motor 1
 #define AIN1 5
 #define AIN2 18
 #define PWMA 19
+
+// Pino stby ponte h
 #define STBY 21
 
+// Pino Encoder
+#define C1 26
+#define C2 27
+byte Encoder_C1Last;
+int duracao;
+boolean Direcao;
+
+
+
+
+
+
 BluetoothSerial SerialBT;
-String valorRecebido;
+String data;
 
 
 // int ledVermelho =  26;
 // int ledAmarelo = 32;
+
+
+void updateEncoder(){
+  int Lstate = digitalRead(C1);
+  if ((Encoder_C1Last == LOW) && Lstate == HIGH){
+    int val = digitalRead(C2);
+    if (val == LOW && Direcao){
+      Direcao = false; //Reverse
+    }
+    else if (val == HIGH && !Direcao){
+      Direcao = true;  //Forward
+    }
+  }
+  Encoder_C1Last = Lstate;
+  if (!Direcao)  duracao++;
+  else  duracao--;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -30,6 +63,16 @@ void setup() {
   pinMode(AIN2, OUTPUT);
   pinMode(PINLED, OUTPUT);
 
+
+  pinMode(C1, INPUT);
+  pinMode(C2, INPUT_PULLUP);
+
+  // digitalWrite(C1, HIGH);
+  // digitalWrite(C2, HIGH);
+
+  attachInterrupt(C1,updateEncoder, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(C2),updateEncoder, CHANGE);
+
   digitalWrite(STBY, HIGH);
   // pinMode(ledVermelho, OUTPUT);
   // pinMode(ledAmarelo, OUTPUT);
@@ -39,69 +82,87 @@ void loop() {
   int i;
 
   if (SerialBT.available()){
-    valorRecebido = SerialBT.readString();
-    Serial.println(valorRecebido);
-    // SerialBT.println(valorRecebido);
-
-    //formato do dado recebido
-    //     kP     ki      kD
-    // 000.000;000.000;000.000
-    if(valorRecebido == "estado_a" || valorRecebido == "Cliente Conectado!" || valorRecebido == "Cliente Desconectado!"){
-      // Serial.println(valorRecebido);
-      SerialBT.print(valorRecebido);
+    data = SerialBT.readString();
+    Serial.println(data);
+    
+    if(data == "a"){
+      for(i=0;i<5;i++){
+      SerialBT.print("Estado de calibração iniciando");
+      }
     }
-    else if(valorRecebido == "ok"){
-      SerialBT.print(valorRecebido);
+    else if(data == "b"){
+      SerialBT.print("Iniciando percurso");
+    }
+    else if(data == "c"){
+      SerialBT.print("Modo Standby");
+    }
+    else if(data == "Cliente Conectado!" || data ==  "Cliente Desconectado!"){
+      SerialBT.print(data);
     }
     else{
-      String stgkP = valorRecebido.substring(0,7);
-      double kP = stgkP.toDouble();
-      String stgkI = valorRecebido.substring(8,15);
-      double kI = stgkI.toDouble();
-      String stgkD = valorRecebido.substring(16,23);
-      double kD = stgkD.toDouble();
-      if((kP + kI + kD) < 100){
-        Serial.println("parece que deu certo essa bucetona aqui");
-      }
+      //formato do dado recebido
+      //           kP     ki      kD     Vel
+      //        d;000.000;000.000;000.000;000
+      //Carac.     2-9     10-17   18-25   26-29
 
-      // prova real de que o numero está chegando inteiro
+      String rawKp = data.substring(2,9);
+      double kP = rawKp.toDouble();
 
-      Serial.print(kP, 6);
-      Serial.print(" | ");
-      Serial.print(kI, 6);
-      Serial.print(" | ");
-      Serial.println(kD, 6);
+      String rawKi = data.substring(10,17);
+      double kI = rawKi.toDouble();
+
+      String rawKd = data.substring(18,25);
+      double kD = rawKd.toDouble();
+
+      String rawSpeed = data.substring(26,29);
+      int speed = rawSpeed.toInt();
+
+      // Prova pra ver se o numero ta virando numero de fato
+      Serial.print("Soma: ");
+      Serial.println(kP+kI+kD+speed);
+
+      // Printa na serial do bt
+      SerialBT.print("Kp: ");
       SerialBT.print(kP);
-
+      SerialBT.print(" | Ki: ");
+      SerialBT.print(kI);
+      SerialBT.print(" | Kd: ");
+      SerialBT.print(kD);
+      SerialBT.print(" | Vel: ");
+      SerialBT.print(speed);
+      
     }
   }
 
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(AIN2, LOW);
+  // digitalWrite(AIN1, HIGH);
+  // digitalWrite(AIN2, LOW);
 
-  for(i=0;i<255;i++){
-    analogWrite(PWMA, i);
-    Serial.println(i);
-    delay(10);
-  }
-  for(i=255;i>0;i--){
-    analogWrite(PWMA, i);
-    Serial.println(i);
-    delay(10);
-  }
+  // for(i=0;i<255;i++){
+  //   analogWrite(PWMA, i);
+  //   Serial.println(duracao);
+  //   delay(30);
+  // }
+  // for(i=255;i>0;i--){
+  //   analogWrite(PWMA, i);
+  //   Serial.println(duracao);
+  //   delay(30);
+  // }
+  // digitalWrite(AIN1, LOW);
+  // digitalWrite(AIN2, HIGH);
 
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
+  // for(i=0;i<255;i++){
+  //   analogWrite(PWMA, i);
+  //   Serial.println(duracao);
+  //   delay(30);
+  // }
+  // for(i=255;i>0;i--){
+  //   analogWrite(PWMA, i);
+  //   Serial.println(duracao);
+  //   delay(30);
+  // }
   
-  for(i=0;i<255;i++){
-    analogWrite(PWMA, i);
-    Serial.println(i);
-    delay(10);
-  }
-  for(i=255;i>0;i--){
-    analogWrite(PWMA, i);
-    Serial.println(i);
-    delay(10);
-  }
+  // delay(100);
+
+
 
 }
